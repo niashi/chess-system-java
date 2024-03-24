cc3f55a16b1d620fd8583b67f1bdf5b5adb05185
Niashi <mathias.niashi@gmail.com>
2024-03-24 16:47:25 -0300

    Possible moves of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -14,4 +14,10 @@ public class King extends ChessPiece {
     public String toString() {
         return super.toString("K");
     }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
+    }
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -14,4 +14,10 @@ public class Rook extends ChessPiece {
     public String toString() {
         return super.toString("R");
     }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
+    }
 }
